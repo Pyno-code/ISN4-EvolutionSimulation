bf94ada22b1fca8da5534849a31d8c56f15e493c
Hugo Patouillard <hugo.patouillard@gmail.com>
2025-05-21 17:36:15 +0200

    create canva for nourriture fix entity (regarde bien noah) fix simulation

--- nourriture.py
+++ nourriture.py
@@ -1,11 +1,30 @@
+from tkinter import Canvas
+
 class Nourriture: 
-    def __init__(self, id, position, energie=0):
-        self.position = position
+    def __init__(self, canvas, x, y, id, energie=0):
         self.energie = energie % 16
+
         self.id = id
+
         self.type = "Nourriture"
+
         self.size = 1
         self.color = self.make_color()
+        self.canvas_color = self.make_canvas_color()
+
+        self.canvas = canvas
+        self.x, self.y = x, y
+        self.square = self.canvas.create_rectangle(x - 10, y - 10, x + 10, y + 10, fill=self.canvas_color)
+
+        self.exist = True
 
     def make_color(self):
         return (90 + int((255-90) * (self.energie/15)**1.7), 255, 93)
+    
+    def make_canvas_color(self):
+        return "#%02x%02x%02x" % self.color
+    
+    def delete(self):
+        if self.exist:
+            self.canvas.delete(self.square)
+            self.exist = False
