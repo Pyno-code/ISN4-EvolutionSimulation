2b4e8c3f7caf0fb1a5069ca44ed90304908102d5
Hugo Patouillard <hugo.patouillard@gmail.com>
2025-06-04 18:04:34 +0200

    fix level

--- backend/entity.py
+++ backend/entity.py
@@ -158,7 +158,7 @@ class Entity:
 
     def update_level(self):
         if self.level < 3:
-            self.level = abs(max(1, min(3, int(self.energy / 7000**(1/1.5)))))
+            self.level = int(max(1, min(3, int(self.energy / 7000**(1/1.5)))))
             self.update_speed()
             self.detection_range = (4 - self.level) * 40
 
