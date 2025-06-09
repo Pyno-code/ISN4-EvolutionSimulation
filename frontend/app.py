711fa4def17c8192e28ab8ad4388f7f43f05c201
Manuel Belanger <manuel.belanger49@gmail.com>
2025-06-09 11:59:43 +0200

    docstrings app.py

--- frontend/app.py
+++ frontend/app.py
@@ -13,7 +13,21 @@ from frontend.graphiques import Graphiques
 
 
 class SimulationInterface(ttk.Window):
+    """
+    Classe représentant l'interface graphique de la simulation évolutive.
+
+    Cette interface gère l'affichage de la simulation, les controles de l'utilisateur (curseurs),
+    les statistiques en temps réel et les graphiques associés.
+
+    Parameters
+    ----------
+    simulation : objet Simulation
+    """
+
     def __init__(self, simulation):
+        """
+        Initialise la fenêtre principale et ses composants graphiques.
+        """
         super().__init__(themename ="flatly")
         self.title("Simulation Évolutive")
         self.state("zoomed")
@@ -34,10 +48,11 @@ class SimulationInterface(ttk.Window):
 
         self.running = True
 
-        # self.after(0, self.init_taille_zone_centrale) # pour avoir la taille en pixel de la zone centrale
-
     
     def update(self):
+        """
+        Met à jour l'affichage de la simulation et les graphiques si la simulation tourne, en fonction des boutons enfoncés.
+        """
         if self.running:
             self.update_info_labels()
             self.display.update()
@@ -47,11 +62,17 @@ class SimulationInterface(ttk.Window):
 
 
     def on_close(self):
+        """
+        Gère la fermeture propre (avec le bouton quitter) de la fenêtre et arrête la boucle principale.
+        """
         print("Fermeture propre")
         self.running = False
         self.destroy()
 
-    def start_simulation(self):
+    def start_simulation(self): # lancé avec le bouton démarrer
+        """
+        Lance une nouvelle simulation, réinitialise les paramètres, désactive les sliders pour verrouiller les paramètres.
+        """
         self.simulation.stop()
         self.simulation.reset()
 
@@ -67,6 +88,9 @@ class SimulationInterface(ttk.Window):
         self.slider_longueur.state(["disabled"])
 
     def update_info_labels(self):
+        """
+        Met à jour les labels affichant le nombre d'entités survivantes et de nourriture.
+        """
         nb_survivants = self.simulation.get_number_entity()
         nb_nourriture = self.simulation.get_number_nourriture()
         
@@ -77,8 +101,10 @@ class SimulationInterface(ttk.Window):
 
         
 
-    def reset_simulation(self):
-
+    def reset_simulation(self): #déclencé par le bouton reeset
+        """
+        Réinitialise la simulation à ses valeurs de départ, réactive les sliders et réinitialise les affichages.
+        """
         self.etat_start = False
         self.simulation.stop()
 
@@ -104,7 +130,11 @@ class SimulationInterface(ttk.Window):
         self.bouton_pause_play.config(text="Pause")
         self.info_content.config(text="Simulation reprise.")
 
-    def toggle_pause_play(self):
+    def toggle_pause_play(self): #déclenché par le bouton pause
+        """
+        Met en pause ou reprend la simulation selon l'état actuel.
+        Affiche un message d'erreur si la simulation n'a pas été démarrée.
+        """
         if not self.etat_start:
             Messagebox.show_warning(
                 "Vous devez d'abord cliquer sur Démarrer.", 
@@ -124,15 +154,22 @@ class SimulationInterface(ttk.Window):
         self.update_info_labels()
 
     def creer_interfaces(self):
-        self.creer_bande_gauche()
-        self.creer_bande_droite()
-        self.creer_zone_centrale()
-        self.creer_bande_bas()
+        """
+        Initialise l'ensemble des éléments de l'interface utilisateur :
+        bandes latérales, zone centrale et zone inférieure.
+        """
+        self.creer_bande_gauche() # les commandes de la simulation (démarrer, pause ...)
+        self.creer_bande_droite() # les graphiques
+        self.creer_zone_centrale() # le canva de la simulation
+        self.creer_bande_bas() # les curseurs à régler
 
 
     def creer_bande_gauche(self):
+        """
+        Crée la bande de gauche contenant les contrôles, les boutons et les informations de simulation sous la forme d'une autre frame.
+        """
         self.interf_gauche = ttk.Frame(self, style="Card.TFrame", borderwidth=1, relief="solid")
-        self.interf_gauche.place(relx=0, rely=0, relwidth=0.15, relheight=1.0)
+        self.interf_gauche.place(relx=0, rely=0, relwidth=0.15, relheight=1.0) # relwidth et relheight sont des pourcentages de la fenetre principale
 
         # Configuration de la colonne pour largeur constante
         self.interf_gauche.columnconfigure(0, weight=1, uniform="a", minsize=120)
@@ -238,8 +275,10 @@ class SimulationInterface(ttk.Window):
         )
         self.bouton_quitter.grid(row=11, column=0, pady=10, padx=10, sticky="ew")
 
-        # rajout  de   creer_zone_centrale, creer_bande_droite, creer_bande_bas
     def creer_zone_centrale(self):
+        """
+        Crée la zone centrale dédiée à l'affichage visuel de la simulation (Canvas).
+        """
         self.zone_centrale = ttk.Frame(self, style="Card.TFrame", borderwidth=1, relief="solid")
         self.zone_centrale.place(relx=0.15, rely=0.0, relwidth=0.55, relheight=0.92)
 
@@ -253,12 +292,18 @@ class SimulationInterface(ttk.Window):
 
 
     def creer_bande_droite(self):
+        """
+        Crée la bande de droite destinée à l'affichage des graphiques.
+        """
         self.interf_droite = ttk.Frame(self, style="Card.TFrame", borderwidth=1, relief="solid")
         self.interf_droite.place(relx=0.7, rely=0, relwidth=0.3, relheight=1.0)
 
         self.graphiques = Graphiques(self.interf_droite, self.simulation)
 
     def creer_bande_bas(self):
+        """
+        Crée la bande inférieure avec les sliders de réglage du nombre d'entités.
+        """
         self.interf_bas = ttk.Frame(self, style="Card.TFrame", borderwidth=1, relief="solid")
         self.interf_bas.place(relx=0.15, rely=0.92, relwidth=0.55, relheight=0.08)
 
@@ -268,7 +313,7 @@ class SimulationInterface(ttk.Window):
         ttk.Label(frame_nb, text="Nb entités:").pack(side="left")
         self.label_nb_value = ttk.Label(frame_nb, text="20", width=5)
         self.label_nb_value.pack(side="left")
-        self.slider_nb = ttk.Scale(
+        self.slider_nb = ttk.Scale( # slider pour le nombre d'entités
             frame_nb,
             from_=1,
             to=100,
@@ -284,9 +329,9 @@ class SimulationInterface(ttk.Window):
         frame_width = ttk.Frame(self.interf_bas)
         frame_width.grid(row=0, column=1, padx=10, pady=30, sticky="ew")
         ttk.Label(frame_width, text="Largeur:").pack(side="left")
-        self.label_width_value = ttk.Label(frame_width, text="800", width=5)
+        self.label_width_value = ttk.Label(frame_width, text="800", width=5) 
         self.label_width_value.pack(side="left")
-        self.slider_largeur = ttk.Scale(
+        self.slider_largeur = ttk.Scale( # slider pour la largeur du canva de simulation
             frame_width,
             from_=100,
             to=1000,
@@ -305,7 +350,7 @@ class SimulationInterface(ttk.Window):
         ttk.Label(frame_length, text="Longueur:").pack(side="left")
         self.label_length_value = ttk.Label(frame_length, text="600", width=5)
         self.label_length_value.pack(side="left")
-        self.slider_longueur = ttk.Scale(
+        self.slider_longueur = ttk.Scale( # slider pour la largeur du canva de simulation
             frame_length,
             from_=100,
             to=900,
@@ -319,19 +364,11 @@ class SimulationInterface(ttk.Window):
         self.slider_longueur.set(600)
         self.slider_longueur.pack(side="left", fill="x", expand=True)
 
-        # Configurer le poids des colonnes dans interf_bas
+        # Configurer le poids des colonnes, pour que les sliders prennent la même place
         self.interf_bas.grid_columnconfigure(0, weight=1)
         self.interf_bas.grid_columnconfigure(1, weight=1)
         self.interf_bas.grid_columnconfigure(2, weight=1)
 
-    def init_taille_zone_centrale(self):
-        self.update_idletasks()  # force un rendu à jour
-        self.largeur_bande_centrale = self.winfo_width()*0.55
-        self.hauteur_bande_centrale = self.winfo_height()*0.92
-        print(f"Taille zone centrale: {self.largeur_bande_centrale}x{self.hauteur_bande_centrale}") 
-
-    
-    
 
 if __name__ == "__main__":
     sim = Simulation(fps=60)
